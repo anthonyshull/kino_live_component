37b923b97cc43031f3fbcbf362905eb7f737da66
Anthony Shull <shull.anthony@gmail.com>
2024-11-06 17:59:07 -0600

    first commit

---
+++ lib/kino_live_view.ex
@@ -0,0 +1,18 @@
+defmodule KinoLiveView do
+  @moduledoc """
+  Documentation for `KinoLiveView`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KinoLiveView.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kino_live_view_test.exs
@@ -0,0 +1,8 @@
+defmodule KinoLiveViewTest do
+  use ExUnit.Case
+  doctest KinoLiveView
+
+  test "greets the world" do
+    assert KinoLiveView.hello() == :world
+  end
+end
